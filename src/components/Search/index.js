6b7be41de6575ee48b61cc2953856b2f94d6de3e
NhanPhan159 <nhandut2019@gmail.com>
2023-11-25 15:55:39 +0700

    search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,5 @@
 import { Input, Button } from "antd";
 export default function Search() {
-
   return (
     <div className=" w-full flex gap-3 items-center">
       <div className="flex flex-col w-1/6 text-black gap-3">
@@ -9,21 +8,20 @@ export default function Search() {
       </div>
       <div className="flex flex-col w-4/6 text-black gap-3">
         <div className="flex gap-2">
-            <Input className="flex-1" placeholder="Insert link heer" />;
+          <Input className="flex-1" placeholder="Insert link heer" />;
         </div>
         <div className="flex gap-2">
-            <Input className="flex-1" placeholder="" />;
+          <Input className="flex-1" placeholder="" />;
         </div>
       </div>
       <div className="flex flex-col w-1/6 text-black gap-3">
-            <Button onClick={getProduct} type="primary" className="bg-red-200">
-                Suggest
-            </Button>
-            <Button type="primary" className="bg-red-200">
-                Suggest
-            </Button>
-        </div>
-
+        <Button type="primary" className="bg-red-200">
+          Suggest
+        </Button>
+        <Button type="primary" className="bg-red-200">
+          Suggest
+        </Button>
+      </div>
     </div>
   );
 }
